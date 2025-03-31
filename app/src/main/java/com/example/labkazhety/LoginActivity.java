708497d428957ed5f7329e933b57761097d78de8
Ad1L2005 <adil.nurgozha@gmail.com>
2025-03-31 19:51:52 +0500

    метод для входа

--- app/src/main/java/com/example/labkazhety/LoginActivity.java
+++ app/src/main/java/com/example/labkazhety/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.labkazhety;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,4 +14,32 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edUsername, edPassword;
     private Button btnLogin, btnSignUp;
     private static final String PREFS_NAME = "UserPrefs";
+
+
+    private void loginUser() {
+        String usernameInput = edUsername.getText().toString().trim();
+        String passwordInput = edPassword.getText().toString().trim();
+
+        if (usernameInput.isEmpty() || passwordInput.isEmpty()) {
+            Toast.makeText(this, "Введите логин и пароль", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        if (!sharedPreferences.contains(usernameInput)) {
+            Toast.makeText(this, "Пользователь не найден, зарегистрируйтесь", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String savedPassword = sharedPreferences.getString(usernameInput, "");
+
+        if (passwordInput.equals(savedPassword)) {
+            Toast.makeText(this, "Вход успешный!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Неверное имя пользователя или пароль", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
